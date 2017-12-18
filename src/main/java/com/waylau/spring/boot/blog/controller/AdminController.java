68ef591dbd8e70f28767047a12eac284473f9871
zhenfeii <zhenfei.huang@gdkyit.com>
2017-12-18 22:06:46 +0800

    2017年12月18日

---
+++ src/main/java/com/waylau/spring/boot/blog/controller/AdminController.java
@@ -0,0 +1,38 @@
+package com.waylau.spring.boot.blog.controller;
+
+import com.waylau.spring.boot.blog.vo.Menu;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 用户控制器.
+ * 
+ * @author <a href="https://waylau.com">Way Lau</a>
+ * @date 2017年2月26日
+ */
+@Controller
+@RequestMapping("/admins")
+public class AdminController {
+
+	/**
+	 * 获取后台管理主页面
+	 * @return
+	 */
+	@GetMapping
+	public ModelAndView listUsers(Model model) {
+		List<Menu> list = new ArrayList<>();
+		list.add(new Menu("用户管理", "/users"));
+		list.add(new Menu("角色管理","roles"));
+		list.add(new Menu("时间管理","times"));
+			model.addAttribute("list", list);
+		return new ModelAndView("/admins/index", "model", model);
+	}
+ 
+	 
+}
